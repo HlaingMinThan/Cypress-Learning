3edd01e0bbeac0db468a70f1c9c03a5a137ad172
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-05-01 16:15:47 +0700

    some useful iframe commands

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -1,28 +1,37 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * Fetch the body of an iFrame.
+ */
+Cypress.Commands.add("getIframe", () => {
+    return cy
+        .get("iframe")
+        .its("0.contentDocument.body", { log: false })
+        .should("not.be.empty")
+        .then((body) => {
+            cy.wrap(body, { log: false });
+        });
+});
+
+/**
+ * Enter the card details for a Stripe elements form.
+ *
+ * @param  {Object} options
+ */
+Cypress.Commands.add("enterStripeCard", (options = {}) => {
+    options = {
+        ...{ approved: true },
+        ...options,
+    };
+
+    cy.getIframe().within(() => {
+        cy.get("[name=cardnumber]")
+            .type(options.approved ? "4242424242424242" : "4000000000000002")
+            .wait(300);
+
+        cy.get("[name=exp-date]").type("0424").wait(300);
+        cy.get("[name=cvc]").type("242").wait(300);
+        cy.get("[name=postal]").type("42424");
+    });
+});
 
 Cypress.Commands.add("writeFixtureByServer", (url, fixturePath) => {
     cy.request(url).as("getServerPosts");
